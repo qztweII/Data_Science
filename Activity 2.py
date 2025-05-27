eb20af915947b9902995344033f149f779cdc3df
qztweII <terencexfwu@gmail.com>
2025-05-27 00:51:54 +0000

    Activity 2

--- Activity 2.py
+++ Activity 2.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-data = [1, 2, 3, 4, 5, 6]
+df = pd.read_csv("athlete_events.csv")
 
-df = pd.
+print(df['Sport'].value_counts().head()) #The top 5 sports were: Athletics, Gymnastics, Swimming, Shooting, Cycling
+print(df['Sex'].value_counts())          #There are 2.638 times more males than females in the olympics (3 d.p.). 
+print(df.describe())                     #Average age: 25 years old, oldest: 97 years, youngest: 10 years
+
+#Country codes:
+#URS = USSR
+#GDR = East Germany
+#FRG = West Germany
